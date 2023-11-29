0666d807ae04a72ef760f47db35bc34ec264039b
Karpovets Ivan <karpovets90@gmail.com>
2023-11-30 00:06:19 +0200

    test task add lowel letter

--- test.app/lib/UserSection/User.php
+++ test.app/lib/UserSection/User.php
@@ -8,26 +8,44 @@ class User
      * @param int $userId
      * @return array
      */
-    public static function getUserNameVowels(int $userId): array
+    public static function getUserNameVowels(int $userId): string
     {
-        $userData =  [];
+        $userData =  '';
+        $vowels = "AEIOUY";
 
         if ($userId > 0)
         {
             $params = [
                 'filter' => ['=ID' => $userId, 'ACTIVE'=> 'Y'],
-                'select' => ['ID', 'NAME', 'LAST_NAME', 'SECOND_NAME'],
+                'select' => ['NAME', 'LAST_NAME', 'SECOND_NAME'],
                 'cache' => ['ttl' => 3600]
             ];
 
             if ($user = \Bitrix\Main\UserTable::getRow($params))
             {
                 foreach ($user as $code=> $val) {
-                    $userData[\ToLower($code)] = $val;
+                    $userData .= self::getVowelsLetters($val);
                 }
             }
         }
 
         return $userData;
+    }
+
+    /**
+     * @param string $word
+     * @return string
+     */
+    private static function getVowelsLetters(string $word): string
+    {
+        $vowelsValue = '';
+        $vowels = ['a','e','i','o','u', 'а', 'у', 'о', 'и', 'э', 'ы', 'і'];
+        $word = \ToLower($word);
+        foreach (mb_str_split($word) as $letter) {
+            if (in_array($letter, $vowels))
+                $vowelsValue .= $letter;
+        }
+
+        return $vowelsValue;
     }
 }
